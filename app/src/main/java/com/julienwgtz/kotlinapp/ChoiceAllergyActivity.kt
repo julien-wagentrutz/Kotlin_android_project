8b450a7b6cd52455bb6c2335d530f2643a56f437
julien Wagentrutz <julien.wagentrutz@hetic.net>
2020-05-30 01:32:34 +0200

    :rocket: Add function to get info from the API by the barcode

--- app/src/main/java/com/julienwgtz/kotlinapp/ChoiceAllergyActivity.kt
+++ app/src/main/java/com/julienwgtz/kotlinapp/ChoiceAllergyActivity.kt
@@ -11,12 +11,6 @@ class ChoiceAllergyActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_choice_allergy)
-        setSupportActionBar(toolbar)
-
-        fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show()
-        }
     }
 
 }
--- app/src/main/java/com/julienwgtz/kotlinapp/MainActivity.kt
+++ app/src/main/java/com/julienwgtz/kotlinapp/MainActivity.kt
@@ -1,12 +1,95 @@
 package com.julienwgtz.kotlinapp
 
+import android.app.Activity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
+import android.view.View
+import android.widget.Toast
+import com.google.zxing.integration.android.IntentIntegrator
+import okhttp3.*
+import org.json.JSONObject
+import java.io.IOException
+
 
 class MainActivity : AppCompatActivity() {
 
+    val aller = true
+    val URL = "https://world.openfoodfacts.org/api/v0/product/"
+    var okHttpClient: OkHttpClient = OkHttpClient()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        if(aller)
+        {
+            setContentView(R.layout.activity_main)
+            val scanner = IntentIntegrator(this)
+            scanner.setDesiredBarcodeFormats(IntentIntegrator.QR_CODE)
+            scanner.setBeepEnabled(false)
+            scanner.initiateScan()
+        }
+        else
+        {
+            startActivity(Intent(this, ChoiceAllergyActivity::class.java))
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.choice_allergy, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId)
+        {
+            R.id.choice->
+            {
+                startActivity(Intent(this, ChoiceAllergyActivity::class.java))
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?)
+    {
+        if (resultCode == Activity.RESULT_OK)
+        {
+            val result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data)
+            if (result != null)
+            {
+                if (result.contents == null)
+                {
+                    Toast.makeText(this, "Nous avons rien trouvé", Toast.LENGTH_LONG).show()
+                }
+                else
+                {
+                    // Requette API
+                    getInfoJsonFromApi("737628064502")
+                }
+            } else
+            {
+                super.onActivityResult(requestCode, resultCode, data)
+            }
+        }
+    }
+
+    private fun getInfoJsonFromApi(codeBarre : String) {
+        val fullUrl = URL + codeBarre + ".json"
+        val request: Request = Request.Builder().url(fullUrl).build()
+        okHttpClient.newCall(request).enqueue(object : Callback {
+            override fun onFailure(call: Call?, e: IOException?) { }
+
+            override fun onResponse(call: Call?, response: Response?) {
+                val json = response?.body()?.string()
+                val content = (JSONObject(json)["code"] as String)
+
+                runOnUiThread {
+                    Log.d("Data",content)
+                }
+            }
+        })
     }
 }
