88f0c9abac32b33e9b6519bc96481c0c098b2618
zoevincent <zoe.vincent@hetic.net>
2020-05-30 17:25:34 +0200

    :rocket: allergies settings with shared preferences start

--- app/src/main/java/com/julienwgtz/kotlinapp/ChoiceAllergyActivity.kt
+++ app/src/main/java/com/julienwgtz/kotlinapp/ChoiceAllergyActivity.kt
@@ -1,10 +1,13 @@
 package com.julienwgtz.kotlinapp
 
+import android.content.Context
+import android.content.SharedPreferences
 import android.os.Bundle
-import com.google.android.material.snackbar.Snackbar
+import android.provider.Settings
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
-
 import kotlinx.android.synthetic.main.activity_choice_allergy.*
+
 
 class ChoiceAllergyActivity : AppCompatActivity() {
 
@@ -13,4 +16,53 @@ class ChoiceAllergyActivity : AppCompatActivity() {
         setContentView(R.layout.activity_choice_allergy)
     }
 
+    fun saveAllergies(saveButton:View){
+        // creation of shared preferences editor
+        val userAllergies = getSharedPreferences("com.julienwgtz.kotlinapp", Context.MODE_PRIVATE).edit()
+        // if an allergy is checked, set it true in shared preferences
+        if (milk_allergy_switch.isChecked){
+            userAllergies.putBoolean("milk_allergy", true)
+        }
+        if (wheat_allergy_switch.isChecked){
+            userAllergies.putBoolean("wheat_allergy", true)
+        }
+        if (mustard_allergy_switch.isChecked){
+            userAllergies.putBoolean("mustard_allergy", true)
+        }
+        // if an allergy is not checked, set it false in shared preferences
+        if (!milk_allergy_switch.isChecked){
+            userAllergies.putBoolean("milk_allergy", false)
+        }
+        if (!wheat_allergy_switch.isChecked){
+            userAllergies.putBoolean("wheat_allergy", false)
+        }
+        if (!mustard_allergy_switch.isChecked){
+            userAllergies.putBoolean("mustard_allergy", false)
+        }
+        // save shared preferences
+        userAllergies.apply()
+        // test
+        showAllergies()
+    }
+
+    // test to see if it works
+    fun showAllergies(){
+        // creation of shared preferences reader
+        val userAllergiesReader = getSharedPreferences("com.julienwgtz.kotlinapp", Context.MODE_PRIVATE)
+
+        var milk_test = userAllergiesReader.getBoolean("milk_allergy", false)
+        if (milk_test){
+            testResult.text = "Allergique au lait"
+        }
+
+        var wheat_test = userAllergiesReader.getBoolean("wheat_allergy", false)
+        if (wheat_test){
+            testResult.text = "Allergique au blé"
+        }
+
+        var mustard_test = userAllergiesReader.getBoolean("mustard_allergy", false)
+        if (mustard_test){
+            testResult.text = "Allergique à la moutarde"
+        }
+    }
 }
