ec9261d8cf4d9d44ac15181e7e3a631d50c2d8a0
julien Wagentrutz <julien.wagentrutz@hetic.net>
2020-06-06 23:12:39 +0200

    :sparkles: get each allergy from product

--- app/src/main/java/com/julienwgtz/kotlinapp/ResultScan.kt
+++ app/src/main/java/com/julienwgtz/kotlinapp/ResultScan.kt
@@ -3,6 +3,7 @@ package com.julienwgtz.kotlinapp
 import android.graphics.drawable.Drawable
 import android.os.Bundle
 import android.provider.AlarmClock.EXTRA_MESSAGE
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -13,6 +14,7 @@ import com.squareup.picasso.Picasso
 import kotlinx.android.synthetic.main.fragment_result_scan.*
 import org.json.JSONObject
 import java.io.InputStream
+import java.lang.reflect.Array
 import java.net.URL
 
 
@@ -31,11 +33,25 @@ class ResultScan : Fragment() {
     override fun onCreateView( inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
         val json = arguments?.getString("bundleValue")
-        val name = (JSONObject(json).getJSONObject("product")["generic_name"] as String)
-        val url = (JSONObject(json).getJSONObject("product")["image_thumb_url"] as String)
+        val codeBar = (JSONObject(json)["code"] as String)
+        val name = (JSONObject(json).getJSONObject("product")["product_name"] as String)
+        val url = (JSONObject(json).getJSONObject("product")["image_url"] as String)
+        val allergensString = (JSONObject(json).getJSONObject("product")["allergens"] as String)
         val view = inflater.inflate(R.layout.fragment_result_scan, container, false)
         Picasso.get().load(url).into(view.findViewById<ImageView>(R.id.imageView4))
         view.findViewById<TextView>(R.id.productName).setText(name)
+        Log.d("allergy",codeBar )
+
+        if(allergensString.length > 0 )
+        {
+            val allergens = allergensString.split(",")
+            for (allergy in allergens)
+            {
+                Log.d("allergy",allergy.substringAfter(":") )
+            }
+        }
+
+
         return view
     }
 
