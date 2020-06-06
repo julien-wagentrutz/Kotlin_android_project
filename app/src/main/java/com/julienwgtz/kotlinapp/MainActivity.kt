887002a3d82bcc8092e3d7e8188775e22af85f0f
julien Wagentrutz <julien.wagentrutz@hetic.net>
2020-06-06 22:36:52 +0200

    :bug: fix bug when product is not found

--- app/src/main/java/com/julienwgtz/kotlinapp/MainActivity.kt
+++ app/src/main/java/com/julienwgtz/kotlinapp/MainActivity.kt
@@ -1,13 +1,21 @@
 package com.julienwgtz.kotlinapp
 
 import android.app.Activity
+import android.app.AlertDialog
+import android.content.Context
+import android.content.DialogInterface
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
+import android.view.View
+import android.widget.ImageView
+import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.google.zxing.integration.android.IntentIntegrator
+import kotlinx.android.synthetic.main.activity_main.*
 import okhttp3.*
 import org.json.JSONObject
 import java.io.IOException
@@ -69,7 +77,7 @@ class MainActivity : AppCompatActivity() {
                 {
                     // Requette API
                     //getInfoJsonFromApi("737628064502")
-                    getInfoJsonFromApi(result.contents)
+                    getInfoJsonFromApi(result.contents,this,this)
                 }
             } else
             {
@@ -78,7 +86,7 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    private fun getInfoJsonFromApi(codeBarre : String ) {
+    private fun getInfoJsonFromApi(codeBarre : String, context : Context,activity: Activity ) {
         val fullUrl = URL + codeBarre + ".json"
         val request: Request = Request.Builder().url(fullUrl).build()
         okHttpClient.newCall(request).enqueue(object : Callback {
@@ -86,24 +94,38 @@ class MainActivity : AppCompatActivity() {
 
             override fun onResponse(call: Call?, response: Response?) {
                 val json = response?.body()?.string()
-                val content = (JSONObject(json)["code"] as String)
-                val name = (JSONObject(json).getJSONObject("product")["generic_name"] as String)
+                if((JSONObject(json)["status_verbose"] as String) == "product found")
+                {
 
-                runOnUiThread {
-                    val parsedValue = json
-                    val bundle = Bundle()
-                    bundle.putString("bundleValue", parsedValue)
-                    val resultScan = ResultScan()
-                    resultScan.setArguments(bundle)
+                    runOnUiThread {
+                        val parsedValue = json
+                        val bundle = Bundle()
+                        bundle.putString("bundleValue", parsedValue)
+                        val resultScan = ResultScan()
+                        resultScan.setArguments(bundle)
+                        val manager = supportFragmentManager
+                        val transaction = manager.beginTransaction()
+                        transaction.replace(R.id.fragment_container,resultScan)
+                        transaction.addToBackStack(null)
 
-                    val manager = supportFragmentManager
-                    val transaction = manager.beginTransaction()
-                    transaction.replace(R.id.fragment_container,resultScan)
-                    transaction.addToBackStack(null)
-
-                    // Finishing the transition
-                    transaction.commitAllowingStateLoss();
+                        // Finishing the transition
+                        transaction.commitAllowingStateLoss();
+                    }
                 }
+                else
+                {
+                    runOnUiThread {
+                        val builder = AlertDialog.Builder(context)
+                        builder.setTitle("Produit inconnu")
+                        builder.setMessage("Désolé nous ne trouvons pas votre produit")
+                        builder.setPositiveButton(android.R.string.yes) { dialog, which ->
+                            val scanner = IntentIntegrator(activity)
+                            scanner.initiateScan()
+                        }
+                        builder.show()
+                    }
+                }
+
 
             }
         })
